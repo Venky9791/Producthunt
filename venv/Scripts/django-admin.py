27bef637003bd502ec6d9389ace62c93d1c4a24f
Venky <bharthivenky76@gmail.com>
2019-07-31 07:17:56 +0100

    First Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Bhuvan_Nithya\PycharmProjects\producthunt\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
